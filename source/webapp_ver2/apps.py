80cdb40e08df57e4ba466d9d52499a364770fcc6
Artur <metis130383@gmail.com>
2020-10-15 16:38:55 +0600

    Сделан второй   этап

---
+++ source/webapp_ver2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WebappVer2Config(AppConfig):
+    name = 'webapp_ver2'
---
+++ source/webapp_ver2/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, render_to_response
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.generic.base import View
+
+
+class IndexView(View):
+    method_decorator(ensure_csrf_cookie)
+    def get(self, request, *args, **kwargs):
+        return render_to_response('index.html')
